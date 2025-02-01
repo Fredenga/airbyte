d9b192b730ddd7c52ea3aeb5379006d474792695
Fredrick Denga <dengafred@gmail.com>
2025-02-01 07:30:08 -0800

    complete Cards section

--- src/components/UpcomingEvents.tsx
+++ src/components/UpcomingEvents.tsx
@@ -1,9 +1,11 @@
+import UpcomingEventsBody from "./UpcomingEventsBody";
 import UpcomingEventsTitle from "./UpcomingEventsTitle";
 
 const UpcomingEvents = () => {
   return (
     <section className="md:px-36">
       <UpcomingEventsTitle />
+      <UpcomingEventsBody />
     </section>
   );
 };
--- src/components/UpcomingEventsBody.tsx
+++ src/components/UpcomingEventsBody.tsx
@@ -4,18 +4,41 @@ const UpcomingEventsBody = () => {
     <section>
       <ul className="grid md:grid-cols-3 gap-4">
         {items.map((item) => (
-          <li>
+          <li className="">
             <div className="">
               <div className="">
                 <img src={`${item.source}`} alt={`${item.title}`} />
               </div>
-              <div className="">
-                <h1>{item.title}</h1>
-                <p>{item.date}</p>
-                <p>{item.venue}</p>
-                <p>{item.price}</p>
+              <div className="flex flex-col items-center bg-black text-white">
+                <h1 className="font-bold text-xl mt-2 mb-3">{item.title}</h1>
+                <p className="mb-3 flex items-center">
+                  <img
+                    className="w-5 h-5 bg-white rounded-full mr-2 "
+                    src="/src/assets/calendar.png"
+                    alt=""
+                  />
+                  {item.date}
+                </p>
+                <p className="mb-3 flex items-center">
+                  <img
+                    className="w-5 h-5 bg-white rounded-full mr-2 "
+                    src="/src/assets/location.png"
+                    alt=""
+                  />
+                  {item.venue}
+                </p>
+                <p className="mb-3 flex items-center">
+                  <img
+                    className="w-5 h-5 bg-white rounded-full mr-2"
+                    src="/src/assets/supermarket.png"
+                    alt=""
+                  />
+                  {item.price}
+                </p>
+                <button className="bg-white text-black mb-6 px-20 py-1 font-semibold">
+                  Get Tickets
+                </button>
               </div>
-              <button>Get Tickets</button>
             </div>
           </li>
         ))}
