0420f5c16cd8b41dcf9d443a57c9cc2f7c0fe57a
Fredrick Denga <dengafred@gmail.com>
2025-02-01 03:43:00 -0800

    develop navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-100 h-24 flex items-center justify-between md:px-20">
+      <div id="left-nav" className="">
+        <img
+          src="/src/assets/logo.jpg"
+          alt="logo"
+          className="h-12 w-12 cursor-pointer"
+        />
+      </div>
+      <div id="middle-nav" className="">
+        <ul className="flex">
+          <li className="mr-10 font-bold">HOME</li>
+          <li className="mr-10">BIO</li>
+          <li className="mr-10">PORTFOLIO</li>
+          <li className="mr-10">HISTORY</li>
+          <li className="mr-10">AIRBYTE FOUNDATION</li>
+        </ul>
+      </div>
+      <div id="right-nav" className="">
+        <button className="bg-black text-white rounded-xl px-6 py-2 cursor-pointer">
+          Explore Events
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
