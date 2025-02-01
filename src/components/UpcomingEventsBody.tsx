d54d57281fc0a69b27a91bc848530d6b65ee4ade
Fredrick Denga <dengafred@gmail.com>
2025-02-01 07:09:14 -0800

    refactor Upcoming Events card list section

--- src/components/UpcomingEventsBody.tsx
+++ src/components/UpcomingEventsBody.tsx
@@ -1,90 +1,24 @@
+import { items } from "../data/items";
 const UpcomingEventsBody = () => {
   return (
     <section>
       <ul className="grid md:grid-cols-3 gap-4">
-        <li>
-          <div className="">
+        {items.map((item) => (
+          <li>
             <div className="">
-              <img src="/src/assets/card1.jpg" alt="" />
+              <div className="">
+                <img src={`${item.source}`} alt={`${item.title}`} />
+              </div>
+              <div className="">
+                <h1>{item.title}</h1>
+                <p>{item.date}</p>
+                <p>{item.venue}</p>
+                <p>{item.price}</p>
+              </div>
+              <button>Get Tickets</button>
             </div>
-            <div className="">
-              <h1>SOUL ULTIMATE TOUR</h1>
-              <p>December 10th 2016</p>
-              <p>Afterglow, Nairobi, Kenya</p>
-              <p>Ksh. 20000/-</p>
-            </div>
-            <button>Get Tickets</button>
-          </div>
-        </li>
-        <li>
-          <div className="">
-            <img src="/src/assets/card2.jpg" alt="" />
-            <div className=""></div>
-            <div className="">
-              <h1>SUMMERTIDES FESTIVAL</h1>
-              <p>July 6th 2019</p>
-              <p>Diani, Ukunda</p>
-              <p>Ksh. 20000/-</p>
-            </div>
-            <button>Get Tickets</button>
-          </div>
-        </li>
-        <li>
-          <div className="">
-            <div className="">
-              <img src="/src/assets/card3.jpg" alt="" />
-            </div>
-            <div className="">
-              <h1>SOUL ULTIMATE TOUR</h1>
-              <p>December 10th 2016</p>
-              <p>Afterglow, Nairobi, Kenya</p>
-              <p>Ksh. 20000/-</p>
-            </div>
-            <button>Get Tickets</button>
-          </div>
-        </li>
-        <li>
-          <div className="">
-            <div className="">
-              <img src="/src/assets/card4.jpg" alt="" />
-            </div>
-            <div className="">
-              <h1>SOUL ULTIMATE TOUR</h1>
-              <p>December 10th 2016</p>
-              <p>Afterglow, Nairobi, Kenya</p>
-              <p>Ksh. 20000/-</p>
-            </div>
-            <button>Get Tickets</button>
-          </div>
-        </li>
-        <li>
-          <div className="">
-            <div className="">
-              <img src="/src/assets/card5.jpg" alt="" />
-            </div>
-            <div className="">
-              <h1>SOUL ULTIMATE TOUR</h1>
-              <p>December 10th 2016</p>
-              <p>Afterglow, Nairobi, Kenya</p>
-              <p>Ksh. 20000/-</p>
-            </div>
-            <button>Get Tickets</button>
-          </div>
-        </li>
-        <li>
-          <div className="">
-            <div className="">
-              <img src="/src/assets/card6.jpg" alt="" />
-            </div>
-            <div className="">
-              <h1>SOUL ULTIMATE TOUR</h1>
-              <p>December 10th 2016</p>
-              <p>Afterglow, Nairobi, Kenya</p>
-              <p>Ksh. 20000/-</p>
-            </div>
-            <button>Get Tickets</button>
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
     </section>
   );
---
+++ src/data/items.ts
@@ -0,0 +1,52 @@
+export const items: Array<CardInfo> = [
+  {
+    title: "SOUL ULTIMATE TOUR",
+    date: "December 10th 2016",
+    venue: "Afterglow, Nairobi, Kenya",
+    price: "Ksh. 20000",
+    source: "/src/assets/card1.jpg",
+  },
+  {
+    title: "SUMMERTIDES FESTIVAL",
+    date: "July 6th 2019",
+    venue: "Diani, Ukunda",
+    price: "Ksh. 20000",
+    source: "/src/assets/card2.jpg",
+  },
+  {
+    title: "SOUL ULTIMATE TOUR",
+    date: "December 10th 2016",
+    venue: "Afterglow, Nairobi, Kenya",
+    price: "Ksh. 20000",
+    source: "/src/assets/card3.jpg",
+  },
+  {
+    title: "SOUL ULTIMATE TOUR",
+    date: "December 10th 2016",
+    venue: "Afterglow, Nairobi, Kenya",
+    price: "Ksh. 20000",
+    source: "/src/assets/card4.jpg",
+  },
+  {
+    title: "SOUL ULTIMATE TOUR",
+    date: "December 10th 2016",
+    venue: "Afterglow, Nairobi, Kenya",
+    price: "Ksh. 20000",
+    source: "/src/assets/card5.jpg",
+  },
+  {
+    title: "SOUL ULTIMATE TOUR",
+    date: "December 10th 2016",
+    venue: "Afterglow, Nairobi, Kenya",
+    price: "Ksh. 20000",
+    source: "/src/assets/card6.jpg",
+  },
+];
+
+interface CardInfo {
+  title: string;
+  date: string;
+  venue: string;
+  price: string;
+  source: string;
+}
