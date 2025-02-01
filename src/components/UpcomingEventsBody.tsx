6d3c20a3c2b620492c31b0cbbfb5ab429c456f9e
Fredrick Denga <dengafred@gmail.com>
2025-02-01 06:59:55 -0800

    make card blueprint

---
+++ src/components/UpcomingEventsBody.tsx
@@ -0,0 +1,93 @@
+const UpcomingEventsBody = () => {
+  return (
+    <section>
+      <ul className="grid md:grid-cols-3 gap-4">
+        <li>
+          <div className="">
+            <div className="">
+              <img src="/src/assets/card1.jpg" alt="" />
+            </div>
+            <div className="">
+              <h1>SOUL ULTIMATE TOUR</h1>
+              <p>December 10th 2016</p>
+              <p>Afterglow, Nairobi, Kenya</p>
+              <p>Ksh. 20000/-</p>
+            </div>
+            <button>Get Tickets</button>
+          </div>
+        </li>
+        <li>
+          <div className="">
+            <img src="/src/assets/card2.jpg" alt="" />
+            <div className=""></div>
+            <div className="">
+              <h1>SUMMERTIDES FESTIVAL</h1>
+              <p>July 6th 2019</p>
+              <p>Diani, Ukunda</p>
+              <p>Ksh. 20000/-</p>
+            </div>
+            <button>Get Tickets</button>
+          </div>
+        </li>
+        <li>
+          <div className="">
+            <div className="">
+              <img src="/src/assets/card3.jpg" alt="" />
+            </div>
+            <div className="">
+              <h1>SOUL ULTIMATE TOUR</h1>
+              <p>December 10th 2016</p>
+              <p>Afterglow, Nairobi, Kenya</p>
+              <p>Ksh. 20000/-</p>
+            </div>
+            <button>Get Tickets</button>
+          </div>
+        </li>
+        <li>
+          <div className="">
+            <div className="">
+              <img src="/src/assets/card4.jpg" alt="" />
+            </div>
+            <div className="">
+              <h1>SOUL ULTIMATE TOUR</h1>
+              <p>December 10th 2016</p>
+              <p>Afterglow, Nairobi, Kenya</p>
+              <p>Ksh. 20000/-</p>
+            </div>
+            <button>Get Tickets</button>
+          </div>
+        </li>
+        <li>
+          <div className="">
+            <div className="">
+              <img src="/src/assets/card5.jpg" alt="" />
+            </div>
+            <div className="">
+              <h1>SOUL ULTIMATE TOUR</h1>
+              <p>December 10th 2016</p>
+              <p>Afterglow, Nairobi, Kenya</p>
+              <p>Ksh. 20000/-</p>
+            </div>
+            <button>Get Tickets</button>
+          </div>
+        </li>
+        <li>
+          <div className="">
+            <div className="">
+              <img src="/src/assets/card6.jpg" alt="" />
+            </div>
+            <div className="">
+              <h1>SOUL ULTIMATE TOUR</h1>
+              <p>December 10th 2016</p>
+              <p>Afterglow, Nairobi, Kenya</p>
+              <p>Ksh. 20000/-</p>
+            </div>
+            <button>Get Tickets</button>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default UpcomingEventsBody;
