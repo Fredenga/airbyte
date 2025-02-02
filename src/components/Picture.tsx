3f493109a76d6fd3f03e6bcc64e906095f323c1b
Fredrick Denga <dengafred@gmail.com>
2025-02-02 12:07:15 -0800

    done Hero section

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -2,12 +2,12 @@ const Picture = () => {
   return (
     <div className="w-full relative">
       <img src="/src/assets/hero.jpg" className="h-full w-full" alt="hero" />
-      <h1 className="text-white text-7xl font-bold absolute top-[calc(25%+16.67%)] left-1/16 -translate-y-1/2">
+      <h1 className="text-white xs:text-2xl text-4xl md:text-7xl font-bold absolute top-[calc(25%+16.67%)] left-1/16 -translate-y-1/2">
         Unforgettable <br></br> Experiences, <br></br> Beautifully Delivered{" "}
         <br></br>
         Across Africa
       </h1>
-      <p className="absolute bottom-[calc(10%+16.67%)] text-white left-1/16 text-xl">
+      <p className="hidden xl:block absolute bottom-[calc(10%+16.67%)] text-white left-1/16 text-xl">
         At Airbeat, we are dedicated to delivering exceptional entertainment
         <br></br>
         experiences that leave lasting memories. Join us as we celebrate the
